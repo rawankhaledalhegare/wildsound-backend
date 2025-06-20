10ba480f06712ae0bcdc71211c7e44551e4f3ebf
rawankhaledalhegare <memee4438@gmail.com>
2025-06-20 17:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/api/audio/upload", methods=["POST"])
 def upload_audio():
